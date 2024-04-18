829800bcbffabc09e286885f7653c6d90cc08f41
Umirek <77798569+Umirek@users.noreply.github.com>
2024-04-18 07:47:48 +0000

    added express framework to render html files and to handle email

---
+++ src/app.js
@@ -0,0 +1,69 @@
+require('dotenv').config();
+
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
+const mustacheExpress = require('mustache-express');
+const express = require('express');
+const path = require('path');
+const transporter = nodemailer.createTransport({
+    host: 'mail.gmx.net', // SMTP host
+    port: 587, // SMTP port
+    auth: {
+        user: process.env.EMAIL_ID,
+        pass: process.env.EMAIL_PASSWORD
+    }
+
+});
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('view engine', 'mustache');
+app.engine('mustache', mustacheExpress());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'pages/index.html'));
+})
+
+app.get('/contact', (req, res) => {
+    res.sendFile(path.join(__dirname, 'pages/contact.html'));
+})
+
+app.post('/', (req, res) => {
+    const {firstname, lastname, email, phone, letter, terms} = req.body;
+
+    const mailOptions = {
+        from: process.env.EMAIL_ID,
+        to: process.env.EMAIL_ID,
+        subject: `New Application for  ${matchedJob.title}`,
+        html: `
+        <p><strong>Name:</strong> ${firstname}</p>
+        <p><strong>Name:</strong> ${lastname}</p>
+        <p><strong>Email:</strong> ${email}</p>
+        <p><strong>Phone:</strong> ${phone}</p>
+        <p><strong>Date of Birth:</strong> ${letter}</p>
+        `
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+
+        if (error){
+            console.log(error);
+            res.status(500).send('Error sending email');
+
+        } else {
+            console.log('Email sent: ' + info.response);
+            res.status(200).send('We got your request');
+        }
+    });
+});
+
+
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Server running on https://localhost:${port}`);
+});
